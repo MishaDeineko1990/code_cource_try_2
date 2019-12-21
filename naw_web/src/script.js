2924778a9fb7e424348ac785d941089bc6b1ac63
MishaDeineko1990 <MishaDeineko1990@gmail.com>
2019-12-21 23:44:57 +0300

    hinish header

---
+++ naw_web/src/script.js
@@ -0,0 +1,10 @@
+// paralax top banner 
+
+$(window).scroll(function(){
+
+    let wScroll = $(this).scrollTop();
+
+   $('.logo_txt').css({
+        'transform' : 'translate(0px, '+ wScroll  +'px)'
+   });
+});
