1303e2d49222ddc12aa7635f3a4fa17be86b65b6
MishaDeineko1990 <MishaDeineko1990@gmail.com>
2019-12-26 22:22:33 +0300

    26.12.2019

--- naw_web/src/script.js
+++ naw_web/src/script.js
@@ -6,7 +6,12 @@ $(window).scroll(function(){
 
    $('.logo_txt').css({
      //    'transform' : 'translate(0px, '+wScroll/2+'%)'
+    
    });
+   window.setTimeout(() => {
+    $('.logo_txt').html('<p>brrrrrrrrrggggggggggggrrrrr</p>')
+  }, 2000);
 });
+let b = 1;
 
-
+$('.logo_txt').html('<p>brrrrrrrrrrrrrr</p>')
