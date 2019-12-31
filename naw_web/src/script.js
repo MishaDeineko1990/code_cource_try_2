7c1ceb0a9ab0906a52937d546254f1cffa2087cd
MishaDeineko1990 <MishaDeineko1990@gmail.com>
2019-12-31 19:50:44 +0300

    happy new year

--- naw_web/src/script.js
+++ naw_web/src/script.js
@@ -1,8 +1,27 @@
 // paralax top banner 
 
 $(window).scroll(function(){
-    let wScroll = $(this).scrollTop();
-   $('.logo_txt').css({
+   
+  let wScroll = $(this).scrollTop();
+  
+  
+  // ---------img paralax-------
+  $('.logo_txt').css({
      'transform' : 'translate(0px, '+wScroll/5+'%)'    
    });
+
+
+
+
+if(wScroll > $('.content_section .block_skils').offset().top - ($(window).height() / 1.2)) {
+    $('.block_skils').each(function(i){
+
+      setTimeout(function(){
+        $('.block_skils').eq(i).addClass('is-showing');
+      }, 150 * (i+1));
+    });
+  }
+
+
+
 });  
