a4b60a77dc59b4ccad09b759a2e2d06c657224ad
MishaDeineko1990 <MishaDeineko1990@gmail.com>
2019-12-31 03:17:31 +0300

    big worck

--- naw_web/src/script.js
+++ naw_web/src/script.js
@@ -1,17 +1,8 @@
 // paralax top banner 
 
 $(window).scroll(function(){
-
     let wScroll = $(this).scrollTop();
-
    $('.logo_txt').css({
-     //    'transform' : 'translate(0px, '+wScroll/2+'%)'
-    
+     'transform' : 'translate(0px, '+wScroll/5+'%)'    
    });
-   window.setTimeout(() => {
-    $('.logo_txt').html('<p>brrrrrrrrrggggggggggggrrrrr</p>')
-  }, 2000);
-});
-let b = 1;
-
-$('.logo_txt').html('<p>brrrrrrrrrrrrrr</p>')
+});  
