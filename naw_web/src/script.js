888ac8d38fd28d2b2dcf2379a425f91e239903b0
MishaDeineko1990 <MishaDeineko1990@gmail.com>
2020-01-02 03:58:37 +0300

    crazy night

--- naw_web/src/script.js
+++ naw_web/src/script.js
@@ -7,12 +7,12 @@ $(window).scroll(function(){
   
   // ---------img paralax-------
   $('.logo_txt').css({
-     'transform' : 'translate(0px, '+wScroll/5+'%)'    
+     'transform' : 'translate(0px, '+wScroll/5+'%)'  
    });
 
 
 
-
+// --------shaw block skils---------
 if(wScroll > $('.content_section .block_skils').offset().top - ($(window).height() / 1.2)) {
     $('.block_skils').each(function(i){
 
@@ -21,6 +21,32 @@ if(wScroll > $('.content_section .block_skils').offset().top - ($(window).height
       }, 150 * (i+1));
     });
   }
+
+
+
+
+
+// ------------------footer parala--------------
+  let wih = $('.flag').offset().top - ($(window).height());
+
+
+  if(wScroll>wih) { 
+
+  $('.parallax__layer__5').css({
+    'transform' : 'translate(0px, '+(wScroll -wih)/-18+'%)'
+  });
+
+  $('.parallax__layer__6').css({
+    'transform' : 'translate(0px, '+(wScroll -wih)/-6+'%)'
+  });
+
+  $('.cover_paralax').css({
+    'height' : (10+((wScroll -wih)/1.3))+'px'
+  });
+}
+
+
+
 
 
 
