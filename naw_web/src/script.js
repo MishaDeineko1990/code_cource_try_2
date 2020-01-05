bf2581b33ecedadf94ed1d91e0cee9b25abdd56b
MishaDeineko1990 <MishaDeineko1990@gmail.com>
2020-01-06 01:01:43 +0300

    svg futer and adaptation

--- naw_web/src/script.js
+++ naw_web/src/script.js
@@ -1,5 +1,15 @@
 // paralax top banner 
 
+h_flag = $(window).height()*.150;
+h_footer = $(window).height()*.20;
+
+$('.flag').css({
+  'height' : h_flag +'px'
+});
+$('.footer').css({
+  'height' : h_footer +'px'
+});
+
 $(window).scroll(function(){
    
   let wScroll = $(this).scrollTop();
@@ -32,22 +42,41 @@ if(wScroll > $('.content_section .block_skils').offset().top - ($(window).height
 
   if(wScroll>wih) { 
 
-  $('.parallax__layer__5').css({
-    'transform' : 'translate(0px, '+(wScroll -wih)/-18+'%)'
-  });
+    $('.parallax__layer__5').css({
+      'transform' : 'translate(0px, '+(wScroll-wih)*-.15+'%)'
+    });
 
-  $('.parallax__layer__6').css({
-    'transform' : 'translate(0px, '+(wScroll -wih)/-6+'%)'
-  });
+    $('.parallax__layer__6').css({
+      'transform' : 'translate(0px, '+(wScroll-wih)*-0.6+'%)'
+    });
 
-  $('.cover_paralax').css({
-    'height' : (10+((wScroll -wih)/1.3))+'px'
-  });
+    $('.cover_paralax').css({
+      'height' : (10+((wScroll-wih)*2.8))+'px'
+    });
+
+    $('.pre_paralax').css({
+      'transform' : 'translate(0px, '+(wScroll -wih)*0.7+'%)'
+    });
+
+ 
+  
+}
+
+let wih2 = $('.cover_paralax').offset().top - ($(window).height());
+
+console.log('wscroll   '+wScroll);
+console.log(wih2);
+
+
+
+
+if(wScroll>wih2){
+  $('.nav').addClass('hiden_nav');
 }
 
 
-
-
-
+if(wScroll<wih2){
+  $('.nav').removeClass('hiden_nav');
+}
 
 });  
